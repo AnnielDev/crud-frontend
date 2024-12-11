d874dc2453282ec5895ee3b7d6d8931cee87e72b
Anniel-Reyes <areyes@brisiao.do>
2024-12-11 17:27:54 -0400

    style: public url

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,7 +4,7 @@ const http = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? import.meta.env.VITE_LOCAL_URL
-      : "",
+      : import.meta.env.VITE_PUBLIC_URL,
 });
 
 export default http;
